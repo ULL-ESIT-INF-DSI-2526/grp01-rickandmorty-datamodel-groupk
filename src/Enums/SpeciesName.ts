531c169fd8d18385010f99cf0937ac8190b4fa1b
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-03-16 13:23:32 +0000

    Añadido algunos enumerados

---
+++ src/Enums/SpeciesName.ts
@@ -0,0 +1,12 @@
+export enum SpeciesName {
+    HUMANO = "Humano",
+    CRONENBERG = "Cronenberg",
+    GAZORPIANO = "Gazorpiano",
+    MEESEEKS = "Meeseeks",
+    ZIGERION = "Zigerion",
+    BIRDPERSON = "Birdperson",
+    SQUANCHY = "Squanchy",
+    GROMFLOMITE = "Gromflomite",
+    PLUTONIANO = "Plutoniano",
+    UNITY = "Unity"
+}
---
+++ src/Enums/SpeciesType.ts
@@ -0,0 +1,10 @@
+export enum SpeciesType {
+    HUMANOIDE = "Humanoide",
+    AMORFO = "Amorfo",
+    ROBOTICO ="Robótico",
+    PARASITO = "Parásito",
+    HIVEMIND = "Hivemind",
+    MUTACIONBIOLOGICA = "Mutación biológica",
+    ENTIDADARTIFICIAL = "Entidad artificial",
+    INSECTOIDE = "Insectoide"
+}
