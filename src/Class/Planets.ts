78d3432ed4b6b47f573dc2b12161f9c74a56a495
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-03-16 17:55:39 +0000

    Avanzando en las clases

--- src/Class/Planets.ts
+++ src/Class/Planets.ts
@@ -1,5 +1,12 @@
 import { Dimensions } from "./Dimensions";
 
 export class Planets {
+    private _id: string;
+    private _name: string;
+    private _type: string;
+    private _dimension: Dimensions;
+    private _population: number;
+    private _desc: string;
+
     
 }
--- src/Class/Species.ts
+++ src/Class/Species.ts
@@ -1,14 +1,15 @@
 import { Dimensions } from "./Dimensions";
+import { Planets } from "./Planets";
 
 export class Species {
     private _id: string;
     private _name: string;
-    private _origin: Dimensions;
+    private _origin: Dimensions | Planets;
     private _type: string;
     private _expectancy: number;
     private _desc: string;
 
-    constructor( _id:string, _name: string, _origin: Dimensions, _type: string, _expectancy: number, _desc: string) {
+    constructor( _id:string, _name: string, _origin: Dimensions | Planets, _type: string, _expectancy: number, _desc: string) {
         this._id = _id;
         this._name = _name;
         this._origin = _origin;
