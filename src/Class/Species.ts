4d0dbe8516b3f937e0a867da10cdd6091b759c33
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-03-16 18:08:36 +0000

    Avanzando en las clases

--- src/Class/Species.ts
+++ src/Class/Species.ts
@@ -1,21 +1,18 @@
 import { Dimensions } from "./Dimensions";
 import { Planets } from "./Planets";
+import { Attributes } from "./AbstractAttributes";
 
-export class Species {
-    private _id: string;
-    private _name: string;
+export class Species extends Attributes {
     private _origin: Dimensions | Planets;
     private _type: string;
     private _expectancy: number;
-    private _desc: string;
 
     constructor( _id:string, _name: string, _origin: Dimensions | Planets, _type: string, _expectancy: number, _desc: string) {
-        this._id = _id;
-        this._name = _name;
+        super(_id, _name, _desc);
         this._origin = _origin;
         this._type = _type;
         this._expectancy = _expectancy;
-        this._desc = _desc;
+        
     }
 
     //Completar
