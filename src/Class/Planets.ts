522addefd6b8b69904120a2d777c0e02e9b119bf
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-03-16 17:56:05 +0000

    Avanzando en las clases

--- src/Class/Planets.ts
+++ src/Class/Planets.ts
@@ -8,5 +8,12 @@ export class Planets {
     private _population: number;
     private _desc: string;
 
-    
+    constructor( _id: string, _name: string, _type: string, _dimension: Dimensions, _population: number, _desc: string) {
+        this._id = _id;
+        this._name = _name;
+        this._type = _type;
+        this._dimension = _dimension;
+        this._population = _population;
+        this._desc = _desc;
+    }
 }
