eab6e2b9bc47a57d1ca19d89ee6e4cb39ada3a41
Alejandro Dorta <alu0101478982@ull.edu.es>
2026-03-17 14:10:25 +0000

    Añadir test y cambiar add y remove srvicies

--- src/Servicies/CharacterServicies.ts
+++ src/Servicies/CharacterServicies.ts
@@ -56,4 +56,49 @@ export class CharacterServices {
 
         return true;
     }
+
+    consultCharacterByName(name: string, direcction: boolean ,sort?: number): Character[] {
+        let characters: Character[] = this.characters.filter(character => character.name === name);
+        if (sort == 1) {
+            return this.sortByName(characters);
+        } else if (sort == 2) {
+
+        } else {
+            return characters;
+        }
+    }
+
+    consultCharacterBySpecies(specie: Species): Character[] {
+        return this.characters.filter(character => character.species === specie);
+    }
+
+    consultCharacterByAfilation(afiliation: string): Character[] {
+        return this.characters.filter(character => character.afiliation === afiliation);
+    }
+
+    consultCharacterByState(state: string): Character[] {
+        return this.characters.filter(character => character.state === state);
+    }
+
+    consultCharacterByDimension(dimension: Dimensions): Character[] {
+        return this.characters.filter(character => character.dimension === dimension);
+    }
+
+    sortByName(characters: Character[]): Character[] {
+        if (characters.length === 0) {
+            return characters;
+        }
+        let names: string[] = [];
+        for (let name of Character.name) {
+            names.push(name);
+        }
+        names.sort();
+        let sortCharacters: Character[] = [];
+        // Mirara lo de undifiened
+        for(let i: number = 0; i < names.length; ++i) {
+            const character: Character = characters.find(c => c.name === names[i])!;
+            sortCharacters.push(character);
+        }
+        return sortCharacters;
+    }
 }
