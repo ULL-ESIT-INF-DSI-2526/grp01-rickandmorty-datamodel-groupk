1a774cb567c4e5b512adb16332504d338298d918
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-03-17 13:59:20 +0000

    Añadido algunos comentarios

--- src/Class/Invents.ts
+++ src/Class/Invents.ts
@@ -10,10 +10,10 @@ export class Invents extends Attributes {
    * Constructor para el objeto Invents
    * @param id - ID del invento
    * @param name - Nombre del invento
-   * @param inventor - Personaje que crea el invento
-   * @param type 
-   * @param danger 
-   * @param desc 
+   * @param inventor - Personaje que crea el invento, es del tipo Character
+   * @param type - Tipo del invento
+   * @param danger - Nivel de peligro del invento que va entre 1 y 10
+   * @param desc - Descripción del invento
    */
   constructor(id: string, name: string, inventor: Character, type: string, danger: number, desc: string) {
     super(id, name, desc);
@@ -25,20 +25,38 @@ export class Invents extends Attributes {
     this._dangerlevel = danger;
   }
 
+  /**
+   * Getter para el inventor
+   */
   get inventor(): Character { return this._inventor; }
 
+  /**
+   * Getter para el tipo 
+   */
   get type(): string { return this._type; }
 
+  /**
+   * Getter para el nivel de peligro
+   */
   get dangerLevel(): number { return this._dangerlevel; }
 
+  /**
+   * Setter del inventor
+   */
   set inventor(newInventor: Character) {
     this._inventor = newInventor;
   }
 
+  /**
+   * Setter para el tipo
+   */
   set type(newType: string) {
     this._type = newType;
   }
 
+  /**
+   * Setter para el nivel de peligro
+   */
   set dangerLevel(newDanger: number) {
     if (newDanger < 1 || newDanger > 10) {
       throw new Error("El nivel de peligrosidad debe estar entre 1 y 10");
