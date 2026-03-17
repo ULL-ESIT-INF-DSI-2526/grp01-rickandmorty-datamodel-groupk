1ae765c5cbfd828c9e6d1ea06ab04ddb94826935
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-03-17 13:15:59 +0000

    Añadido algunos comentarios

--- src/Class/AbstractAttributes.ts
+++ src/Class/AbstractAttributes.ts
@@ -1,25 +1,49 @@
+/**
+ * Clase abstracta que contiene elementos comunes de las clases a las que será extendida.
+ */
 export abstract class Attributes {
     private _id: string;
     private _name: string;
     private _desc: string;
 
+    /**
+     * Constructor para la clase abstracta Attributes.
+     * @param id - ID del objeto
+     * @param name - Nombre del objeto
+     * @param desc - Descripción del objeto
+     */
     constructor(id: string, name: string, desc: string) {
         this._id = id;
         this._name = name;
         this._desc = desc;
     }
 
+    /**
+     * Getter de ID
+     */
     get id(): string { return this._id; }
 
+    /**
+     * Getter del nombre
+     */
     get name(): string { return this._name; }
 
+    /**
+     * Getter de la descripción
+     */
     get desc(): string { return this._desc; }
 
+    /**
+     * Setter del nombre
+     */
     set name(name: string) {
         if (!name) throw new Error("El nombre no puede estar vacío");
         this._name = name;
     }
 
+    /**
+     * Setter de la descripción
+     */
     set desc(desc: string) {
         this._desc = desc;
     }
--- src/Class/Character.ts
+++ src/Class/Character.ts
@@ -2,6 +2,9 @@ import { Species } from "./Species";
 import { Dimensions } from "./Dimensions";
 import { Attributes } from "./AbstractAttributes";
 
+/**
+ * Clase para definir los personajes que extiende a la clase abstracta Attributes
+ */
 export class Character extends Attributes{
     private _species: Species;
     private _dimension: Dimensions;
@@ -9,6 +12,17 @@ export class Character extends Attributes{
     private _afiliation: string;
     private _iq: number;
 
+    /**
+     * Constructor del objeto Character
+     * @param id - ID del personaje
+     * @param name - Nombre del personaje
+     * @param species - Especie del personaje, lo cual es del tipo Species
+     * @param dimension - Dimensión a la que pertenece, lo cual es del tipo Dimensions
+     * @param state - Estado del personaje
+     * @param afiliation - Afiliación del personaje
+     * @param iq - IQ del personaje entre 1 y 10
+     * @param desc - Descripción del personaje
+     */
     constructor( id:string, name: string, species: Species, dimension: Dimensions, state: string, afiliation: string, iq: number, desc: string) {
         super(id, name, desc);
         this._species = species;
@@ -21,32 +35,62 @@ export class Character extends Attributes{
         this._iq = iq;
     }
 
+    /**
+     * Getter para la especie
+     */
     get species(): Species { return this._species; }
 
+    /**
+     * Getter para la dimensión
+     */
     get dimension(): Dimensions { return this._dimension; }
 
+    /**
+     * Getter para el estado
+     */
     get state(): string { return this._state; }
 
+    /**
+     * Getter para la afiliación
+     */
     get afiliation(): string { return this._afiliation; }
 
+    /**
+     * Getter para el IQ
+     */
     get iq(): number { return this._iq; }
 
+    /**
+     * Setter para la especie
+     */
     set species(newSpec: Species) {
         this._species = newSpec;
     }
 
+    /**
+     * Setter para la dimensión
+     */
     set dimension(newDim: Dimensions) {
         this._dimension = newDim;
     }
 
+    /**
+     * Setter para el estado
+     */
     set state(newState: string) {
         this._state = newState;
     }
 
+    /**
+     * Setter para la afiliación
+     */
     set afiliation(newAfil: string) {
         this._afiliation = newAfil;
     }
 
+    /**
+     * Setter para el IQ
+     */
     set iq(newIQ: number) {
         if (newIQ < 1 || newIQ > 10) {
             throw new Error("El nivel de inteligencia debe estar entre 1 y 10");
