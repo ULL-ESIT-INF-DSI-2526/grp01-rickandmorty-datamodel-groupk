adb5239006a661f0c3cc510c3089ed7c69bdc404
GonzaloBarroso <alu0101137094@ull.edu.es>
2026-03-20 17:02:20 +0000

    LocationServices, DimensionServices, InventServices y sus test hechos con base de datos

---
+++ src/Servicies/Multiverse.ts
@@ -0,0 +1,32 @@
+import { Low } from "lowdb";
+import { Data } from "../DataBase/db.js";
+
+import { LocationServices } from "./LocationServicies.js";
+import { DimensionServices } from "./DimensionServices.js";
+import { InventServices } from "./InventServices.js";
+import { CharacterServices } from "./CharacterServicies.js";
+
+export class MultiverseManager {
+    private _db: Low<Data>;
+    public dimensions: DimensionServices;
+    //public characters: CharacterServices; //Descomentar cuando esté completa la clase CharacterServices
+    public localitations: LocationServices;
+    public invents: InventServices;
+
+    /**
+     * El constructor toma la base de datos como parámetro y la usa para inicializar los "Services",
+     * de esta forma el gestor tiene acceso a todo el multiverso
+     * @param dataBase - Base de datos
+     */
+    constructor(dataBase: Low<Data>) {
+        this._db = dataBase;
+        this.dimensions = new DimensionServices(dataBase);
+        //this.characters = new CharacterServices(dataBase); //Descomentar cuando esté completa la clase CharacterServices
+        this.localitations = new LocationServices(dataBase);
+        this.invents = new InventServices(dataBase);
+    }
+
+    /*
+    Aquí hay que poner las funciones que se piden para el gestor en el guión
+    */
+}
